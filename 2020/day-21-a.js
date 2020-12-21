3e7c7beafb13ecb82ee322872d0698b5836cc3dc
Mark van Cuijk <mark.van.cuijk@alliander.com>
2020-12-21 09:27:43 +0100

    Solution to puzzle 21

---
+++ 2020/day-21-a.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+
+const ingredientsPerAllergen = {};
+const allIngredients = fs.readFileSync('./inputs/21/real.txt', { encoding: 'utf8' })
+	.split('\n')
+	.map(line => line.match(/([a-z ]+) \(contains ([a-z, ]+)/))
+	.map(([_, ingredients, allergens]) => [ingredients.split(' '), allergens.split(', ')])
+	.flatMap(([ingredients, allergens]) => {
+		allergens.forEach(allergen => {
+			if (ingredientsPerAllergen.hasOwnProperty(allergen)) {
+				ingredientsPerAllergen[allergen].push(ingredients);
+			} else {
+				ingredientsPerAllergen[allergen] = [ingredients];
+			}
+		});
+		return ingredients;
+	});
+
+const ingredientForAllergen = {};
+while (Object.keys(ingredientForAllergen).length < Object.keys(ingredientsPerAllergen).length) {
+	Object.entries(ingredientsPerAllergen).forEach(([allergen, ingredients]) => {
+		const ingredientOptions = [...new Set(ingredients.flat())]
+			.filter(ingredient => !Object.values(ingredientForAllergen).includes(ingredient))
+			.filter(ingredient => ingredients.every(list => list.includes(ingredient)));
+		if (ingredientOptions.length === 1) {
+			ingredientForAllergen[allergen] = ingredientOptions[0];
+		}
+	});
+}
+
+const safeIngredients = [...new Set(allIngredients)]
+	.filter(ingredient => !Object.values(ingredientForAllergen).includes(ingredient));
+
+const result = allIngredients.filter(ingredient => safeIngredients.includes(ingredient)).length;
+
+console.log(result);
