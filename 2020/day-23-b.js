9281e539eb7eb16a7954945fd69347ed2ebe7ab2
Mark van Cuijk <mark.van.cuijk@alliander.com>
2020-12-23 14:35:43 +0100

    Remove superfluous prev on nodes for puzzle 23b

--- 2020/day-23-b.js
+++ 2020/day-23-b.js
@@ -3,7 +3,6 @@ let firstCups = '135468729'.split('').map(num => ({ num: Number.parseInt(num) })
 
 let cupOne, cupNine;
 firstCups.forEach((cup, i) => {
-	cup.prev = firstCups[(i + 8) % 9];
 	cup.next = firstCups[(i + 1) % 9];
 	cup.minusOne = firstCups.filter(cup2 => cup2.num === cup.num - 1)[0];
 	if (cup.num === 1) cupOne = cup;
@@ -16,7 +15,6 @@ let prevCup = cupNine;
 for (let i = 10; i <= 1000000; i++) {
 	const newCup = {
 		num: i,
-		prev: prevCup,
 		minusOne: prevCup
 	};
 
@@ -30,8 +28,6 @@ for (let i = 10; i <= 1000000; i++) {
 }
 
 prevCup.next = currentCup;
-currentCup.prev = prevCup;
-
 cupOne.minusOne = prevCup;
 
 for (let i = 0; i < 10000000; i++) {
@@ -40,7 +36,7 @@ for (let i = 0; i < 10000000; i++) {
 	while (moveCups.includes(behindCup)) behindCup = behindCup.minusOne;
 
 	// Do the move
-	[currentCup.next, behindCup.next, moveCups[2].next, moveCups[2].next.prev, moveCups[0].prev, behindCup.next.prev] = [moveCups[2].next, moveCups[0], behindCup.next, currentCup, behindCup, moveCups[2]];
+	[currentCup.next, behindCup.next, moveCups[2].next] = [moveCups[2].next, moveCups[0], behindCup.next];
 	currentCup = currentCup.next;
 }
 
