1d04c48925ddd3dfb0ee4e88c0f7b2f827c8e63f
Mark van Cuijk <github@mark.van-cuijk.nl>
2022-12-11 10:48:49 +0100

    Little refactoring

--- 2022/src/main/kotlin/net/phedny/aoc2022/Day11.kt
+++ 2022/src/main/kotlin/net/phedny/aoc2022/Day11.kt
@@ -1,27 +1,26 @@
 package net.phedny.aoc2022
 
-data class Monkey(val id: Int, val items: List<Long>, val operation: (Long) -> Long, val test: (Long) -> Boolean, val trueTo: Int, val falseTo: Int, val inspections: Long) {
+data class Monkey(val id: Int, val items: List<Long>, val operation: (Long) -> Long, val divisibleBy: Long, val trueTo: Int, val falseTo: Int, val inspections: Long = 0) {
     fun catch(item: Long): Monkey = copy(items = items + item)
     fun throwAll(): Monkey = copy(items = emptyList(), inspections = inspections + items.size)
-    fun distribute(mod: Int, relieve: Boolean): List<Pair<Long, Int>> = items.map { if (relieve) operation(it) / 3 else operation(it) }.map { Pair(it % mod, if (test(it)) trueTo else falseTo) }
+    fun distribute(mod: Long, relieve: Boolean): List<Pair<Long, Int>> = items.map { if (relieve) operation(it) / 3 else operation(it) }.map { Pair(it % mod, if (it % divisibleBy == 0L) trueTo else falseTo) }
 }
 
 fun main() {
-    val (monkeys, divs) = readInput().split("\n\n")
+    val monkeys = readInput().split("\n\n")
         .map { "Monkey .*(\\d):.*items: ([\\d, ]+).*new = old ([+*]) (\\d+|old).*by (\\d+).*monkey (\\d+).*monkey (\\d+)".toRegex(RegexOption.DOT_MATCHES_ALL).matchEntire(it)!!.groupValues }
         .map { groups ->
-            Pair(Monkey(
+            Monkey(
                 groups[1].toInt(),
                 groups[2].split(", ").map { it.toLong() },
-                operation(groups[3], if (groups[4] == "old") null else groups[4].toLong()),
-                divisibleBy(groups[5].toLong()),
+                if (groups[4] == "old") selfOperation(groups[3]) else operation(groups[3], groups[4].toLong()),
+                groups[5].toLong(),
                 groups[6].toInt(),
-                groups[7].toInt(),
-                0
-            ), groups[5].toInt()) }
-        .unzip()
+                groups[7].toInt()
+            )
+        }
 
-    val mod = divs.reduce(Int::times)
+    val mod = monkeys.map(Monkey::divisibleBy).reduce(Long::times)
     listOf(Pair(20, true), Pair(10000, false)).forEach {(rounds, relieve) ->
         generateSequence(monkeys) { doRound(it, mod, relieve) }
             .drop(rounds)
@@ -32,17 +31,21 @@ fun main() {
     }
 }
 
-fun operation(operator: String, operand: Long?): (Long) -> Long =
+fun operation(operator: String, operand: Long): (Long) -> Long =
     when (operator) {
-        "+" -> fun(it): Long = it + (operand ?: it)
-        "*" -> fun(it): Long = it * (operand ?: it)
+        "+" -> fun(it): Long = it + operand
+        "*" -> fun(it): Long = it * operand
         else -> throw IllegalArgumentException("unsupported operator")
     }
 
-fun divisibleBy(divBy: Long): (Long) -> Boolean =
-    fun(it): Boolean = (it % divBy) == 0L
+fun selfOperation(operator: String): (Long) -> Long =
+    when (operator) {
+        "+" -> fun(it): Long = 2 * it
+        "*" -> fun(it): Long = it * it
+        else -> throw IllegalArgumentException("unsupported operator")
+    }
 
-fun doRound(monkeys: List<Monkey>, mod: Int, relieve: Boolean): List<Monkey> =
+fun doRound(monkeys: List<Monkey>, mod: Long, relieve: Boolean): List<Monkey> =
     monkeys.indices.fold(monkeys) { monkeyList, current ->
         val thrown = monkeyList[current].distribute(mod, relieve)
         monkeyList.mapIndexed { index, monkey ->
