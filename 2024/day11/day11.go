9c3ba43ed05650bbda08b998e51d3e2a1ba5429d
Mark van Cuijk <github@mark.van-cuijk.nl>
2024-12-11 10:18:21 +0100

    Day 11

---
+++ 2024/day11/day11.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"aoc2024/util"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	m := make(map[string]int)
+	for _, s := range strings.Split(util.ReadLines()[0], " ") {
+		m[s]++
+	}
+	m, partA := stepsAndTally(m, 25)
+	fmt.Println(partA)
+	_, partB := stepsAndTally(m, 50)
+	fmt.Println(partB)
+}
+
+func stepsAndTally(m map[string]int, n int) (map[string]int, int) {
+	for range n {
+		m = step(m)
+	}
+	var tally int
+	for _, n := range m {
+		tally += n
+	}
+	return m, tally
+}
+
+func step(in map[string]int) map[string]int {
+	out := make(map[string]int)
+	for s, n := range in {
+		if s == "0" {
+			out["1"] += n
+		} else if i := len(s); i%2 == 0 {
+			out[s[:i/2]] += n
+			s = s[i/2:]
+			for len(s) > 1 && s[0] == '0' {
+				s = s[1:]
+			}
+			out[s] += n
+		} else {
+			num, _ := strconv.Atoi(s)
+			out[strconv.Itoa(2024*num)] += n
+		}
+	}
+	return out
+}
